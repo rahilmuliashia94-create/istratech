1f15ecee6898b7de04b7d3628f09ce0c48a01be4
rahilmuliashia94-create <rahilmuliashia94@gmail.com>
2026-03-06 15:28:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,8 +82,41 @@ l.x+=0.4
 if(l.x>canvas.width) l.x=0
 })
 
-requestAnimationFrame(draw)
+requestAnimationFrame(draw
+                     
+  
+// Apple style 3D tilt cards
 
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+card.addEventListener("mousemove", (e) => {
+
+const rect = card.getBoundingClientRect();
+
+const x = e.clientX - rect.left;
+const y = e.clientY - rect.top;
+
+const centerX = rect.width / 2;
+const centerY = rect.height / 2;
+
+const rotateX = -(y - centerY) / 12;
+const rotateY = (x - centerX) / 12;
+
+card.style.transform =
+`rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
+
+});
+
+card.addEventListener("mouseleave", () => {
+
+card.style.transform =
+"rotateX(0) rotateY(0) scale(1)";
+
+});
+
+});
 }
 
 draw()
